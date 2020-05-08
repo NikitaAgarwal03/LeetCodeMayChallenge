1734166d2c3f9753e366c319af7a92703d5a08e5
Nikita Agarwal <nikagr0311@gmail.com>
2020-05-09 03:45:20 +0530

    Resolved

--- JewelsAndStones.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def numJewelsInStones(self, J, S):
-        """
-        :type J: str
-        :type S: str
-        :rtype: int
-        """
-        count = sum(s in J for s in S)
-        return count
