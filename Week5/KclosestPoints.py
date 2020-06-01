bdce034e6edb071ab48fadb040477ab2ee582264
Nikita <nikagr0311@gmail.com>
2020-06-02 00:04:10 +0530

    30 May challenge

---
+++ Week5/KclosestPoints.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        points.sort(key = lambda K: K[0]**2 + K[1]**2) 
+        return points[:K] 
